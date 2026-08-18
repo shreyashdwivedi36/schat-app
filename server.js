0d4380c8f3fef786427614a58f5bebd915f17b03
Shreyash Dwivedi <shreyashdwivedi1626@gmail.com>
2026-08-18 21:53:13 +0530

    feat: add digital asset links to prove ownership of twa and remove chrome browser UI

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: '1mb' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { dotfiles: 'allow' }));
 
 
 const https = require('https');
